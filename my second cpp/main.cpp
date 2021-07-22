3c98c217ba6f6dd7c90b5a63a20d566730d78bf0
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2021-07-22 12:23:33 +0700

    add task 3

--- my second cpp/main.cpp
+++ my second cpp/main.cpp
@@ -31,51 +31,59 @@ int main(int argc, const char * argv[]) {
 //        std::cout << "Kelompok Luar Biasa\n";
 //    }
     
-    int month;
+//    int month;
+//
+//    std::cout << "Masukkan urutan bulan\n";
+//
+//    std::cin >> month;
+//
+//    switch (month) {
+//      case 1:
+//        std::cout << "January";
+//        break;
+//      case 2:
+//        std::cout << "February";
+//        break;
+//      case 3:
+//        std::cout << "March";
+//        break;
+//      case 4:
+//        std::cout << "April";
+//        break;
+//      case 5:
+//        std::cout << "May";
+//        break;
+//      case 6:
+//        std::cout << "June";
+//        break;
+//      case 7:
+//        std::cout << "July";
+//        break;
+//      case 8:
+//        std::cout << "August";
+//        break;
+//        case 9:
+//            std::cout << "September";
+//            break;
+//        case 10:
+//            std::cout << "October";
+//            break;
+//        case 11:
+//            std::cout << "November";
+//            break;
+//        case 12:
+//            std::cout << "December";
+//            break;
+//        default:
+//            std::cout << "Tidak ada bulan yang dipilih";
+//    }
     
-    std::cout << "Masukkan urutan bulan\n";
-    
-    std::cin >> month;
-    
-    switch (month) {
-      case 1:
-        std::cout << "January";
-        break;
-      case 2:
-        std::cout << "February";
-        break;
-      case 3:
-        std::cout << "March";
-        break;
-      case 4:
-        std::cout << "April";
-        break;
-      case 5:
-        std::cout << "May";
-        break;
-      case 6:
-        std::cout << "June";
-        break;
-      case 7:
-        std::cout << "July";
-        break;
-      case 8:
-        std::cout << "August";
-        break;
-        case 9:
-            std::cout << "September";
-            break;
-        case 10:
-            std::cout << "October";
-            break;
-        case 11:
-            std::cout << "November";
-            break;
-        case 12:
-            std::cout << "December";
-            break;
-        default:
-            std::cout << "Tidak ada bulan yang dipilih";
+    int i = 1;
+    while (i <= 10) {
+        if (i % 2 == 0) {
+            std::cout << i << ", ";
+        }
+        i++;
     }
     
     return 0;
